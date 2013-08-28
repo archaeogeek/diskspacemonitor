63ba24c1c3556ad50ece3487bdee27ae0d4c55ec
Jo Cook <jocook@astuntechnology.com>
2013-08-28 07:56:00 +0100

    Test and edit linux folder options

--- diskspace.py
+++ diskspace.py
@@ -1,4 +1,4 @@
-import os,sys
+import os
 import platform
 import ctypes
 import smtplib
@@ -16,37 +16,36 @@ config = {
     },
     
     "disk":{
-		"folder": "C:", #use windows convention eg C: or linux \
-		"limit" : "4" #size in GB at which you'd like alert to be fired
-		}
+        "folder": '/', #use windows convention eg C: or linux /
+        "limit" : "4" #size in GB at which you'd like alert to be fired
+        }
 }
 
 
 def get_free_space():
-	""" Return folder/drive free space (in bytes) and email if less than a defined limit"""
-	folder = config["disk"]["folder"]
-	limit = config["disk"]["limit"]
-		
-	print "Folder is %s" % folder
-	if platform.system() == 'Windows':
-		free_bytes = ctypes.c_ulonglong(0)
-		ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes))
-		freegb = free_bytes.value/(1024*1024*1024)
-		#return free_bytes.value
-		print "free space is %s GB" % freegb
-	else:
-		#return os.statvfs(folder).f_bfree
-		freegb = os.statvfs(folder).f_bfree/(1024*1024)
-		print "free space is %s GB" % freegb
-			
-	if int(freegb) <= int(limit):
-		send_alert(folder)
-		
+    """ Return folder/drive free space (in bytes) and email if less than a defined limit"""
+    folder = config["disk"]["folder"]
+    limit = config["disk"]["limit"]
+        
+    print "Folder is %s" % folder
+    if platform.system() == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes))
+        freegb = free_bytes.value/(1024*1024*1024)
+        #return free_bytes.value
+        print "free space is %s GB" % freegb
+    else:
+        #return os.statvfs(folder).f_bfree
+        freegb = os.statvfs(folder).f_bfree/(1024*1024)
+        print "free space is %s GB" % freegb
+            
+    if int(freegb) <= int(limit):
+        send_alert(folder)
+        
 def send_alert(folder):
-	"""send an email"""
-	
+    """send an email"""
+    
     local_vars = locals()
-
     email_subject = config["email"]["subject"] % local_vars
     email_message = config["email"]["message"] % local_vars
 
